095f44a3363da5cd0cc212d3caf826e0348c888e
Hanjeonguk <wjddnr1358@naver.com>
2024-01-12 18:36:28 +0900

    초기 커밋.

---
+++ 01.List/Program.cs
@@ -0,0 +1,299 @@
+﻿// 사용자에게 숫자를 입력 받아서
+// 0부터 숫자까지 가지는 리스트 만들기
+// 0요소를 제거
+// 리스트가 가지는 모든 요소들을 출력해주는 반복분 작성
+namespace List.task
+{
+    public class Program
+    {
+        static void Main1(string[] args)
+        {
+            List<int> list = new List<int>();
+
+            Console.Write("숫자를 입력하세요 : ");
+            int input = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= input; i++)
+            {
+                list.Add(i);
+            }
+            list.Remove(0);
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.Write($"{list[i]}  ");
+            }
+        }
+    }
+}
+// 사용자의 입력을 받아서 없는 데이터면 추가, 있던 데이터면 삭제하는 보관함
+// ex) 1,6,7,8,3 들고 있던 상황이면
+// 2입력하면 1,6,7,8,3,2
+// 7 입력하면 1,6 8,3
+namespace Task24._01._11__2
+{
+    public class Program
+    {
+        static void Main1(string[] args)
+        {
+
+            List<int> list = new List<int>();
+            list.Add(1);
+            list.Add(6);
+            list.Add(7);
+            list.Add(8);
+            list.Add(3);
+
+            Console.Write("숫자를 입력하세요 : ");
+            int input = int.Parse(Console.ReadLine());
+
+
+            bool b = list.IndexOf(input) != -1;
+
+
+            if (b == true)
+            {
+                list.Remove(input);
+            }
+            else
+            {
+                list.Add(input);
+            }
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.Write($"{list[i]} ");
+            }
+        }
+    }
+}
+
+// 인벤토리 구현(아이템 수집, 아이템 버리기)
+
+
+// 긁어온 내용 임시보관 //
+
+
+//namespace Task24._01._11__3
+//{
+
+
+//    using System;
+//    using System.Collections.Generic;
+
+//    class Item
+//    {
+//        public string Name { get; set; }
+//        public int Quantity { get; set; }
+
+//        public Item(string name, int quantity)
+//        {
+//            Name = name;
+//            Quantity = quantity;
+//        }
+//    }
+
+//    class Player
+//    {
+//        public string Name { get; set; }
+//        public Inventory Inventory { get; set; }
+
+//        public Player(string name)
+//        {
+//            Name = name;
+//            Inventory = new Inventory();
+//        }
+//    }
+
+//    class Inventory
+//    {
+//        private List<Item> items;
+
+//        public Inventory()
+//        {
+//            items = new List<Item>();
+//        }
+
+//        public void AddItem(string itemName, int quantity)
+//        {
+//            Item existingItem = items.Find(i => i.Name == itemName);
+
+//            if (existingItem != null)
+//            {
+//                existingItem.Quantity += quantity;
+//            }
+//            else
+//            {
+//                items.Add(new Item(itemName, quantity));
+//            }
+//        }
+
+//        public void UseItem(string itemName, int quantity)
+//        {
+//            Item item = items.Find(i => i.Name == itemName);
+
+//            if (item != null && item.Quantity >= quantity)
+//            {
+//                item.Quantity -= quantity;
+//                Console.WriteLine($"Player used {quantity} {itemName}(s).");
+//            }
+//            else
+//            {
+//                Console.WriteLine($"Not enough {itemName} in the inventory.");
+//            }
+//        }
+
+//        public void DisplayInventory()
+//        {
+//            Console.WriteLine($"Inventory:");
+
+//            foreach (var item in items)
+//            {
+//                Console.WriteLine($"{item.Name}: {item.Quantity}");
+//            }
+//        }
+//    }
+
+//    class Program
+//    {
+//        static void Main()
+//        {
+//            Player player = new Player("John");
+
+//            player.Inventory.AddItem("Health Potion", 5);
+//            player.Inventory.AddItem("Sword", 1);
+//            player.Inventory.AddItem("Shield", 2);
+
+//            player.Inventory.DisplayInventory();
+
+//            player.Inventory.UseItem("Health Potion", 3);
+//            player.Inventory.UseItem("Sword", 1);
+//            player.Inventory.UseItem("Arrow", 5);
+
+//            player.Inventory.DisplayInventory();
+//        }
+//    }
+
+//}
+
+//namespace Task24._01._11__3
+//{
+//    // 기본 아이템 클래스
+//    class Item
+//    {
+//        public string Name { get; set; }
+//        public int Quantity { get; set; }
+
+//        public Item(string name, int quantity)
+//        {
+//            Name = name;
+//            Quantity = quantity;
+//        }
+
+//        public virtual void Use()
+//        {
+//            Console.WriteLine($"Used {Quantity} {Name}(s).");
+//        }
+//    }
+
+//    // 소모품 아이템 클래스
+//    class ConsumableItem : Item
+//    {
+//        public ConsumableItem(string name, int quantity) : base(name, quantity)
+//        {
+//        }
+
+//        public override void Use()
+//        {
+//            base.Use();
+//            Console.WriteLine($"{Name} has been consumed.");
+//        }
+//    }
+
+//    // 장비 아이템 클래스
+//    class EquipmentItem : Item
+//    {
+//        public EquipmentItem(string name, int quantity) : base(name, quantity)
+//        {
+//        }
+
+//        public override void Use()
+//        {
+//            base.Use();
+//            Console.WriteLine($"{Name} has been equipped.");
+//        }
+//    }
+
+//    // 플레이어 클래스
+//    class Player
+//    {
+//        public string Name { get; set; }
+//        public List<Item> Inventory { get; set; }
+
+//        public Player(string name)
+//        {
+//            Name = name;
+//            Inventory = new List<Item>();
+//        }
+
+//        public void AddItem(Item item)
+//        {
+//            Item existingItem = Inventory.Find(i => i.Name == item.Name);
+
+//            if (existingItem != null)
+//            {
+//                existingItem.Quantity += item.Quantity;
+//            }
+//            else
+//            {
+//                Inventory.Add(item);
+//            }
+//        }
+
+//        public void UseItem(string itemName, int quantity)
+//        {
+//            Item item = Inventory.Find(i => i.Name == itemName);
+
+//            if (item != null && item.Quantity >= quantity)
+//            {
+//                item.Quantity -= quantity;
+//                item.Use();
+//            }
+//            else
+//            {
+//                Console.WriteLine($"Not enough {itemName} in the inventory.");
+//            }
+//        }
+
+//        public void DisplayInventory()
+//        {
+//            Console.WriteLine($"Inventory for {Name}:");
+
+//            foreach (var item in Inventory)
+//            {
+//                Console.WriteLine($"{item.Name}: {item.Quantity}");
+//            }
+//        }
+//    }
+
+//    class Program
+//    {
+//        static void Main()
+//        {
+//            Player player = new Player("Alice");
+
+//            player.AddItem(new ConsumableItem("Health Potion", 5));
+//            player.AddItem(new EquipmentItem("Sword", 1));
+//            player.AddItem(new ConsumableItem("Mana Potion", 3));
+
+//            player.DisplayInventory();
+
+//            player.UseItem("Health Potion", 3);
+//            player.UseItem("Sword", 1);
+//            player.UseItem("Arrow", 5);
+
+//            player.DisplayInventory();
+//        }
+//    }
+//}
+
---
+++ Homework/Program.cs
@@ -0,0 +1,90 @@
+﻿//사용자의 입력으로 숫자를 입력 받아서
+//음수는 앞에 추가하고, 양수는 뒤에 추가하여
+//음수&양수를 반으로 나누는 연결리스트 구현
+//입력 받을 때마다 처음부터 끝까지 출력 진행
+using System;
+using System.Runtime.Serialization.Formatters;
+using System.Xml.Linq;
+
+namespace LinkedList.Task
+{
+    public class Program
+    {
+        static void Main1(string[] args)
+        {
+            LinkedList<int> linkedList = new LinkedList<int>();
+            int num = 0;
+            int min = 0;
+            while (num != null)
+            {
+                Console.Write("\n숫자(양수, 음수)를 입력하세요 : ");
+                num = int.Parse(Console.ReadLine());
+                if (num > 0)
+                {
+                    LinkedListNode<int> node = linkedList.AddLast(num);
+                    Console.WriteLine("[입력 받은 수]");
+                    foreach (int element in linkedList) { Console.Write(element); }
+
+                    if (min == 0)
+                    {
+                        min = num;
+                    }
+                    else if (min >= num)
+                    {
+                        min = num;
+                    }
+                }
+                else if (num < 0)
+                {
+                    LinkedListNode<int> node = linkedList.AddFirst(num);
+                    Console.WriteLine("[입력 받은 수]");
+                    foreach (int element in linkedList) { Console.Write(element); }
+                }
+                else // 양수 음수 사이에 0 집어넣기??
+                {
+                    LinkedListNode<int> findNode = linkedList.Find(min);
+                    LinkedListNode<int> node = linkedList.AddAfter(findNode, num);
+
+                    Console.WriteLine("[입력 받은 수]");
+                    foreach (int element in linkedList) { Console.Write(element); }
+                }
+            }
+        }
+    }
+}
+//요세푸스 순열 문제
+
+//1번부터 n번까지 n명의 사람이 원을 이루면서 앉아있고,
+//양의 정수 k(<=n)가 주어진다. 이제 순서대로 k번째 사람을 제거한다.
+//한 사람이 제거되면 남은 사람들로 이루어진 원을 따라
+//이 과정을 계속해 나간다. 이 과정은 n명의 사람이 모두 제거될 때까지
+//계속된다. 원에서 사람들이 제거되는 순서를 (N, K)-요세푸스 순열이라고 한다.
+//예를 들어 (7, 3)-요세푸스 순열은 <3, 6, 2, 7, 5, 1, 4>이다.
+
+//N과 K가 주어지면 (N, K)-요세푸스 순열을 구하는 프로그램을 작성하시오.
+
+namespace LinkedList.Task2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<int> linkedList = new LinkedList<int>();
+            Console.Write("사람의 수를 입력하세요: ");
+            int num = int.Parse(Console.ReadLine());
+            Console.Write("제거할 순서를 입력하세요: ");
+            int kill = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < num; i++)
+            {
+                linkedList.AddLast(i + 1);
+            }
+
+
+
+
+
+        }
+
+    }
+}
