03d5a4f10ced48d7d2a3d0a6dd2f93d585cd7c42
Hanjeonguk <wjddnr1358@naver.com>
2024-01-19 13:12:30 +0900

    test

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace ConsoleApp1
+{
+    public class CheatKey
+    {
+        private Dictionary<string, Action> cheatDic;
+
+        public CheatKey() 
+        {
+            cheatDic = new Dictionary<string, Action>();
+
+
+        }
+
+        public void Run(string cheatKey)
+        {
+
+        }
+
+        private void ShowMeTheMoney()
+        {
+            Console.WriteLine("골드를 얻습니다, 치트키 발동!");
+        }
+
+        private void ThereIsNoCowLevel()
+        {
+            Console.WriteLine("바로 승리합니다, 치트키 발동!");
+        }
+    }
+
+    public class Test
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
