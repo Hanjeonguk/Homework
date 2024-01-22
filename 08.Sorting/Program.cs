3d17ac077338d541a30cb2d9fc7f6a20f8170df2
Hanjeonguk <wjddnr1358@naver.com>
2024-01-22 18:23:32 +0900

    docs: Sorting

---
+++ 08.Sorting/Program.cs
@@ -0,0 +1,52 @@
+﻿//알고리즘 정렬 구현하기
+//1.선택정렬
+//2.삽입정렬
+//3.병합정렬
+//4.버블정렬
+//5.퀵 정렬
+//6.힙 정렬
+//7.안정정렬과 불안정정렬(병합정렬의 사용의의)
+namespace _08.Sorting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> selectionList = new List<int>();
+            List<int> insetionList = new List<int>();
+
+            Random random = new Random();
+            int count = 10;
+
+
+            Console.Write("랜덤 함수:");
+            for (int i = 0; i < count; i++) 
+            {
+                int rand = random.Next(0,100);
+                Console.Write($"{rand,3}");
+                selectionList.Add(rand);
+                insetionList.Add(rand);
+            }
+            Console.WriteLine();
+
+
+
+            Sorting.SelectionSort(selectionList, 0, selectionList.Count);
+            Console.Write("선택 정렬:");
+            foreach(int i in selectionList)
+            {
+                Console.Write($"{i,3}");
+            }
+            Console.WriteLine();
+
+            Sorting.InsertionSort(insetionList,0, insetionList.Count);
+            Console.Write("삽입 정렬:");
+            foreach (int i in insetionList)
+            {
+                Console.Write($"{i,3}");
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
---
+++ 08.Sorting/Sorting.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08.Sorting
+{
+    internal class Sorting
+    {
+        //1.선택정렬
+        public static void Swap(IList<int>list, int left, int right)
+        {
+            int temp = list[left];
+            list[left] = list[right];
+            list[right] = temp;
+        }
+        public static void  SelectionSort(IList<int>list, int start, int end)
+        {
+            for (int i = start; i < end; i++) 
+            {
+                int index = i;
+                for(int j = i+1; j < end; j++)
+                {
+                    if (list[index] > list[j])
+                    {
+                        index = j;
+                    }
+                }
+                Swap(list, i, index);
+            }
+        }
+
+        //2.삽입 정렬
+         public static void InsertionSort(IList<int>list, int start, int end)
+        {
+            for(int i = start; i < end; i++)
+            {
+                for(int j=i; j > 0; j--)
+                {
+                    if (list[j-1] < list[j])
+                    {
+                        break;
+                    }
+                    Swap(list, j - 1, j);
+                }
+            }
+        }   
+        
+    }
+}
