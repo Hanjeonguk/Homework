2799dc5e4f47e456dec22b3919629e285e66ec69
Hanjeonguk <wjddnr1358@naver.com>
2024-01-23 18:13:35 +0900

    .

---
+++ 09. Searching/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Immutable;
+
+namespace _09._Searching
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 3, 5, 7, 9, 8, 6, 4, 2, 0 };
+            foreach (int i in array)
+            {
+                Console.Write($"{i,3}");
+            }
+            Console.WriteLine();
+            Console.Write("순차 탐색: ");
+            Console.WriteLine($"{Searching.SequentialSearch(array, 4)}");
+     
+            Array.Sort( array );
+            Console.WriteLine("정렬");
+            foreach (int i in array)
+            {
+                Console.Write($"{i,3}");
+            }
+            Console.WriteLine();
+            Console.Write("이진 탐색: ");
+            Console.WriteLine($"{Searching.BinarySearch(array,4)}") ;
+
+
+            bool[,] graph = new bool[8, 8]
+               {
+                { false,  true, false, false, false, false, false, false },
+                {  true, false,  true, false, false,  true, false, false },
+                { false,  true, false, false,  true,  true, false, false },
+                { false, false, false, false, false,  true, false, false },
+                { false, false,  true, false, false, false,  true,  true },
+                { false,  true,  true,  true, false, false, false, false },
+                { false, false, false, false,  true, false, false, false },
+                { false, false, false, false,  true, false, false, false },
+               };
+
+            Searching.DFS(in graph, 0, out bool[] dvisited, out int[] dparents);
+            PrintGraphSearch(dvisited, dparents);
+        }
+        public static void PrintGraphSearch(bool[] visited, int[] parents)
+        {
+            for (int i = 0; i < visited.Length; i++)
+            {
+                Console.WriteLine($"{i,5}{visited[i],5}{parents[i],5}");
+            }
+        }
+    }
+}
---
+++ 09. Searching/Searching.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09._Searching
+{
+    internal class Searching
+    {
+        //순차탐색
+        public static int SequentialSearch<T>(in IList<T> list, in T item)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i].Equals(item))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+
+        //이진탐색
+        public static int BinarySearch<T>(in IList<T>list, in T item)where T : IComparable<T>
+        {
+            int low = 0;
+            int high = list.Count - 1;
+
+            while (low <= high)
+            {
+                int middle = (low + high) / 2;
+                int compare = list[middle].CompareTo(item);
+
+                if (compare > 0)
+                    high = middle-1;
+                else if (compare < 0)
+                    low = middle+1;
+                else
+
+                return middle;
+            }
+            return -1;
+        }
+
+        //깊이 우선 탐색
+        public static void DFS(in bool[,] graph, int start, out bool[] visited, out int[] parents)
+        {
+            visited = new bool[graph.GetLength(0)];
+            parents = new int[graph.GetLength(0)];
+
+            for (int i = 0;i < graph.GetLength(0); i++)
+            {
+                visited[i] = false;
+                parents[i] = -1;
+            }
+
+            SearchNode(graph, start, visited, parents);
+        }
+
+        public static void SearchNode(in bool[,] graph, int start, bool[] visited, int[] parents)
+        {
+            visited[start] = true;
+            for(int i = 0; i < graph.GetLength(0); i++)
+            {
+                if (graph[start, i] && !visited[i])
+                {
+                    parents[i] = start;
+                    SearchNode(graph, i, visited, parents);
+                }
+            }
+        }
+
+
+
+
+
+
+
+        //넓이 우선 탐색
+    }
+}
