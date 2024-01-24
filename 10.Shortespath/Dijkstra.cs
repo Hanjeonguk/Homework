12facd5a33ccee5b8ff18199fce1f5e48bf0e28a
Hanjeonguk <wjddnr1358@naver.com>
2024-01-24 18:22:13 +0900

    docs: ShortestPath

---
+++ 10.Shortespath/Dijkstra.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10.Shortespath
+{
+    internal class Dijkstra
+    {
+        const int INF = 99999;
+        public static void ShortestPath(in int[,]graph, in int start, out bool[] visited, out int[] distance, out int[] parents)
+        {
+            visited = new bool[graph.GetLength(0)];
+            distance = new int[graph.GetLength(0)];
+            parents = new int[graph.GetLength(0)];
+
+            for (int i = 0; i < graph.GetLength(0); i++)
+            {
+                visited[i] = false;
+                distance[i] = INF;
+                parents[i] = -1;
+            }
+            distance[start] = 0;
+
+            for (int i = 0;i < graph.GetLength(0); i++)
+            {
+                // 1. 방문하지 않은 정점 중 가장 가까운 정점부터 탐색
+                int next = -1;
+                int minDistance = INF;
+
+                for (int j = 0; j < graph.GetLength(0); j++)
+                {
+                    if (!visited[j] && distance[j] < minDistance)
+                    {
+                        next= j;
+                        minDistance = distance[j];
+                    }
+                }
+                if(next < 0)
+                {
+                    break;
+                }
+
+                // 2. 직접연결된 거리보다 다른 정점을 거쳐서 더 짧아진다면 갱신.
+                for(int j=0; j < graph.GetLength(0); j++)
+                {
+                    if (distance[j] > distance[next] + graph[next, j])
+                    {
+                        distance[j] = distance[next] + graph[next, j];
+                        parents[j] = next;
+                    }
+                }
+                visited[next] = true;
+
+            }
+        }
+       
+                
+            
+        
+
+               
+                
+                
+    }
+}
+
---
+++ 10.Shortespath/Program.cs
@@ -0,0 +1,51 @@
+﻿using _10.Shortespath;
+
+namespace _11._ShortesPath
+{
+    internal class Program
+    {
+        const int INF = 99999;
+
+        static void Main(string[] args)
+        {
+            int[,] graph = new int[8, 8]
+            {
+                { INF, INF, INF, 5, 4, INF, INF, INF },
+                { INF, INF, 6, INF, INF, INF, INF, INF },
+                { 6, INF, INF, INF, INF, 8, INF, INF},
+                { INF, INF, INF,INF, INF, 6, INF, INF },
+                { INF, INF, 6, INF, INF, INF, INF, INF},
+                { INF, INF, INF, INF, INF, INF, 4, 6},
+                { INF, INF, 9, INF, INF, 1, INF, INF},
+                { INF, INF, INF, INF, INF, INF, INF, INF },
+
+            };
+
+            Dijkstra.ShortestPath(in graph, 0, out bool[] visited, out int[] distance, out int[] parents);
+
+            Console.WriteLine("<Dijkstra>");
+            PrintDijkstra(distance, parents);
+        }
+
+        private static void PrintDijkstra(int[] distance, int[] parents)
+        {
+            Console.WriteLine($"{"Vertex",8}{"Distance",10}{"Parents",10}");
+
+            for (int i = 0; i < distance.Length; i++)
+            {
+                Console.Write($"{i,8}");
+
+                if (distance[i] >= INF)
+                {
+                    Console.Write($"{"INF",10}");
+                }
+                else
+                {
+                    Console.Write($"{distance[i],10}");
+                }
+
+                Console.WriteLine($"{parents[i],10}");
+            }
+        }
+    }
+}
